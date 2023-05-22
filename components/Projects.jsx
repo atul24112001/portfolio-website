aa9b02e12d109d782bbd8531b2152ae90a4c0eeb
atul-morchhlay <atulmorchhlay204@gmail.com>
2023-05-23 00:18:29 +0530

    General commit

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -20,35 +20,35 @@ export default function Projects({ projects = [], type }) {
                 {data.map((project, i) => {
                     const src = type + "Project" + i;
                     return (
-                        <Link href={project.url} target='_blank' key={`${i + 1}`}>
-                            <div className='flex flex-col ease-in-out  border-[2px] rounded-md border-primary-color hover:scale-105 transition-all hover:shadow-3xl hover:shadow-primary-lite-color cursor-pointer'>
-                                <Image
-                                    width={500}
-                                    height={500}
-                                    quality={100}
-                                    alt={src}
-                                    src={`/Images/${src}.png`}
-                                    className='w-[100%]  h-auto rounded-t-md '
-                                />
-                                <div className='p-2 border-t-primary-color border-t-[2px]  flex flex-col flex-1 justify-between gap-2'>
-                                    <div className='flex items-center gap-1 justify-between'>
-                                        <h3 className='font-bold text-xl'>{project.title}</h3>
-                                        <div className='flex items-center gap-1'>
-                                            <LinkIcons tooltip="Website" Icon={RiLink} link={project.url} color="#6a8aff" />
-                                            <LinkIcons tooltip="Github" Icon={RiGithubFill} link={project.github} color="#ccc" />
-                                        </div>
-                                    </div>
-                                    <p>{project.description}</p>
-                                    <div className='flex flex-wrap gap-1'>
-                                        {project.stack.map((s, i) => {
-                                            return (
-                                                <Chip font="xs" key={`${i + 1}`} title={s} />
-                                            )
-                                        })}
+                        // <Link href={project.url} target='_blank' key={`${i + 1}`}>
+                        <div key={`${i + 1}`} className='flex flex-col ease-in-out  border-[2px] rounded-md border-primary-color hover:scale-105 transition-all hover:shadow-3xl hover:shadow-primary-lite-color cursor-pointer'>
+                            <Image
+                                width={500}
+                                height={500}
+                                quality={100}
+                                alt={src}
+                                src={`/Images/${src}.png`}
+                                className='w-[100%]  h-auto rounded-t-md '
+                            />
+                            <div className='p-2 border-t-primary-color border-t-[2px]  flex flex-col flex-1 justify-between gap-2'>
+                                <div className='flex items-center gap-1 justify-between'>
+                                    <h3 className='font-bold text-xl'>{project.title}</h3>
+                                    <div className='flex items-center gap-1'>
+                                        <LinkIcons tooltip="Website" Icon={RiLink} link={project.url} color="#6a8aff" />
+                                        <LinkIcons tooltip="Github" Icon={RiGithubFill} link={project.github} color="#ccc" />
                                     </div>
                                 </div>
+                                <p>{project.description}</p>
+                                <div className='flex flex-wrap gap-1'>
+                                    {project.stack.map((s, i) => {
+                                        return (
+                                            <Chip font="xs" key={`${i + 1}`} title={s} />
+                                        )
+                                    })}
+                                </div>
                             </div>
-                        </Link>
+                        </div>
+                        // </Link>
                     )
                 })}
             </div>
