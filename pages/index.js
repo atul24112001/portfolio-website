7a1e51e51443388b2d0ff94f9f5ffc3e4f0873f9
atul24112001 <atulmorchhlay204@gmail.com>
2024-01-11 18:13:19 +0530

    Added freelancing project

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ export default function Home() {
     <main className="w-screen h-screen overflow-auto bg-[#252525] p-2 border-[0px] border-primary-color">
       <NavBar details={details} />
       <Introduction details={details} />
-      <Projects projects={details.projects} type="Advance" />
+      <Projects assetFolder={"Freelance"} title="Freelance Projects" projects={details.projects} type="Advance" />
+      <Projects assetFolder={"Personal"} title="Personal Projects" projects={details.personalProjects} type="Advance" />
     </main>
 
   )
